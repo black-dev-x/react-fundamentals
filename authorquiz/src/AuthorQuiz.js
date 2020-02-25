0d6ed74ac71fe6832836b8fa20105aa828f75519
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-25 18:33:54 -0300

    even more mock data

--- authorquiz/src/AuthorQuiz.js
+++ authorquiz/src/AuthorQuiz.js
@@ -1,21 +1,51 @@
-import React, { useState } from 'react';
-import Continue from './components/Continue';
-import Turn from './components/Turn';
-import Hero from './components/Hero';
-import './AuthorQuiz.css';
+import React, { useState } from 'react'
+import Continue from './components/Continue'
+import Turn from './components/Turn'
+import Hero from './components/Hero'
+import './AuthorQuiz.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Footer from './components/Footer';
+import Footer from './components/Footer'
 
-const authors = [{
-  name: 'Mark Twain',
-  imageUrl: '/images/authors/marktwain.jpg',
-  imageSource: 'Wikimedia Commons',
-  books: [
-    'The Adventures of Huckleberry Finn',
-    'Life on the Mississippi',
-    'Roughing It'
-  ]
-}]
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.png',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: ['The Shining', 'IT']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  }
+]
 
 export default () => {
 
