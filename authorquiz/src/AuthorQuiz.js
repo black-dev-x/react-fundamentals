8ae9ab1e3001df3634c6bdffa78b8f0c554bce65
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-25 17:21:10 -0300

    first components

--- authorquiz/src/AuthorQuiz.js
+++ authorquiz/src/AuthorQuiz.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Continue from './components/Continue';
+import Turn from './components/Turn';
+import Hero from './components/Hero';
 import './AuthorQuiz.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 function AuthorQuiz() {
   return (
-    <div>Author Quiz</div>
+    <div className="container-fluid">
+      <Hero></Hero>
+      <Turn></Turn>
+      <Continue></Continue>
+    </div>
   );
 }
 
---
+++ authorquiz/src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div className="row">
+            <div className="jumbotron col-10 offset-1">
+                <h1>Author Quiz</h1>
+                <p>Select the book written by the author shown</p>
+            </div>
+        </div>
+    )
+}
