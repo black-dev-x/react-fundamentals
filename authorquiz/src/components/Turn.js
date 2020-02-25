64b325f368db318d22d6af3eb61807172fdd84bf
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-25 19:10:15 -0300

    format and class changes

--- authorquiz/src/components/Turn.js
+++ authorquiz/src/components/Turn.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Book from './Book';
+import React from 'react'
+import Book from './Book'
 import './Turn.css'
 
-export default ({author, books}) => {
+export default ({ author, books }) => {
     return (
-        <div className="row turn" style={{backgroundColor: "white"}}>
+        <div className="row turn" style={{ backgroundColor: "white" }}>
             <div className="col-4 offset-1">
-                <img src={author.imageUrl} className="author-image" alt="Author" />
+                <img src={author.imageUrl} className="author-image img-fluid" alt="Author" />
             </div>
             <div className="col-6">
                 {books.map(title => <Book title={title} key={title}></Book>)}
