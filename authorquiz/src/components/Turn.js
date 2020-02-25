6afecb366674bd018e99b39a04b722e606a79579
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-25 17:31:21 -0300

    turn component

--- authorquiz/src/components/Turn.js
+++ authorquiz/src/components/Turn.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-export default props => {
+export default ({author, books}) => {
     return (
-        <h1>Turn</h1>
+        <div className="row turn" style={{backgroundColor: "white"}}>
+            <div className="col-4 offset-1">
+                <imr src={author.imageUrl} className="authorimage" alt="Author" />
+            </div>
+            <div className="col-6">
+                {books.map(title => <p>{title}</p>)}
+            </div>
+        </div>
     )
 }
