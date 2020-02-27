12e22eac118f68429ec44136fa4e72db56ed5f88
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-26 23:06:19 -0300

    first test and adding new dependencies for enzyme

--- authorquiz/src/AuthorQuiz.test.js
+++ authorquiz/src/AuthorQuiz.test.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
-import AuthorQuiz from './AuthorQuiz';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AuthorQuiz from './AuthorQuiz'
+import Enzyme, { mount, shallow, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
 
-test('renders author quiz', () => {
-  const { getByText } = render(<AuthorQuiz />);
-  const title = getByText(/Author Quiz/i);
-  expect(title).toBeInTheDocument();
-});
-
-test('renders without crashing' , () => {
+test('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<AuthorQuiz></AuthorQuiz>, div)
+})
+
+test('When no answer has been selected', () => {
+  const authorQuiz = mount(<AuthorQuiz></AuthorQuiz>)
+  expect(authorQuiz.find("div.row.turn").props().style.backgroundColor).toBe('')
 })
