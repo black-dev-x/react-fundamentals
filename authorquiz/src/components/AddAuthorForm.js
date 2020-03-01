16db1e6bcd5093170dfa8f50911f1886b32e1f70
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-29 23:22:38 -0300

    creating a few form fields

--- authorquiz/src/components/AddAuthorForm.js
+++ authorquiz/src/components/AddAuthorForm.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './AddAuthorForm.css'
 
+const defaultState = {
+    name: '',
+    imageUrl: ''
+}
 export default ({ match }) => {
-
+    const [formState, setFormState] = useState(defaultState)
+    const updateState = event => {
+        const newFormState = { ...formState }
+        const inputName = event.target.name
+        newFormState[inputName] = event.target.value
+        setFormState(newFormState)
+    }
     return (
-        <div>
+        <div className="addAuthorForm">
             <h1>Add Author</h1>
-            <p>{JSON.stringify(match)}</p>
+            <form>
+                <div className="inputGroup">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={formState.name} onChange={updateState}></input>
+                </div>
+                <div className="inputGroup">
+                    <label htmlFor="imageUrl">Image URL</label>
+                    <input type="text" name="imageUrl" value={formState.imageUrl} onChange={updateState}></input>
+                </div>
+            </form>
         </div>
     )
 }
